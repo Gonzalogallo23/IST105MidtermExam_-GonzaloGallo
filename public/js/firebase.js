429927eb4e549015c0cb1d111c381e2fde52e76f
Gonzalo Gallo <gonzalogallot23@gmail.com>
2024-10-21 17:38:31 -0700

    asd

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkM70h1b78oUUmykta_SCk5tyx81bA1Xo",
+  authDomain: "ist105midtermexam-gonzalogallo.firebaseapp.com",
+  projectId: "ist105midtermexam-gonzalogallo",
+  storageBucket: "ist105midtermexam-gonzalogallo.appspot.com",
+  messagingSenderId: "644996123823",
+  appId: "1:644996123823:web:f11b40aac5b5074a34185a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ public/js/index.js
@@ -0,0 +1,67 @@
+window.addEventListener('load', function () {
+
+    document.getElementById('sign-in-button').addEventListener('click', function () {
+
+        let provider = new firebase.auth.GoogleAuthProvider();
+
+        provider.addScope('email');
+        firebase.auth().signInWithPopup(provider)
+            .then(function (result) {
+                console.log('Logging sucessfully', result.user);
+                location.href = 'cultural.html';
+            })
+            .catch(function (error) {
+                console.log('Logging fail', error);
+            });
+    });
+
+    document.getElementById('sign-in-2').addEventListener('click', function () {
+
+        let emailTxt = document.getElementById('email').value;
+        let passtxt = document.getElementById('password').value;
+
+        firebase.auth().signInWithEmailAndPassword(emailTxt, passtxt)
+            .then((userCredential) => {
+                // Signed in
+                let user = userCredential.user;
+                // ...
+                console.log('Logging sucessfully');
+                alert('Logging sucessfully');
+                location.href = 'cultural.html';
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                alert('Logging fail');
+                console.log('Logging fail', errorMessage);
+            });
+
+    });
+
+    document.getElementById('log-in-phonenumber').addEventListener('click', function () {
+        const appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container')
+
+
+        appVerifier.verify().then(function() {
+            document.getElementById('phone-number').classList.remove('d-none');
+            const phoneNumber = document.getElementById('phone-number').value; 
+
+            firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+                .then((confirmationResult) => {
+                    const code = prompt('Enter the verification code you received via SMS:', '');
+                    return confirmationResult.confirm(code); 
+                })
+                .then((result) => {
+                    const user = result.user;
+                    console.log('Logging successfully with phone number:', user);
+                    alert('Logging successfully with phone number');
+                    location.href = 'culturalconnections.html';
+                })
+                .catch((error) => {
+                    console.error('Error during phone number sign-in:', error);
+                });
+        }).catch(function(error) {
+            console.error('Error during reCAPTCHA verification:', error);
+        });
+    });
+});
